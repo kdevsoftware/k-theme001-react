89247d57821d3f803c6169af515041dc3f380a3d
kdev <kdev.software1110@gmail.com>
2019-08-21 04:42:23 -0400

    display main demopage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Main from './DemoPages/Main';
 
-ReactDOM.render(<div>Config start!</div>, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+const renderApp = Component => {
+  ReactDOM.render(<Component />, rootElement);
+};
+
+renderApp(Main);
 
 serviceWorker.unregister();
