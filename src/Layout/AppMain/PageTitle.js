b8e888b1ac99711b5954133b93704d70da635600
kdev <kdev.software1110@gmail.com>
2019-08-21 12:58:58 -0400

    dashboard page main structure

---
+++ src/Layout/AppMain/PageTitle.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PageTitle extends Component {
+  render() { 
+    return (
+      <div>PageTitle works!</div>
+    );
+  }
+}
+ 
+export default PageTitle;
