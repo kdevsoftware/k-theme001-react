fdf3871b025a25d477522649f00a78e573c50754
kdev <kdev.software1110@gmail.com>
2019-08-21 06:26:35 -0400

    app logo component visualization

--- src/Layout/AppLogo/index.js
+++ src/Layout/AppLogo/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Hamburger from 'react-hamburgers';
 
 class HeaderLogo extends React.Component {
   render() {
-    return <div>HeaderLogo works!</div>;
+    return (
+      <Fragment>
+        <div className="app-header__logo">
+          <div className="logo-src" />
+
+          <div className="header__pane ml-auto">
+            <div>
+              <Hamburger />
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
