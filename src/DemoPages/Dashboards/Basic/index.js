193a4459ae520c7742def2ab355fb7ca77a7ff5b
kdev <kdev.software1110@gmail.com>
2019-08-21 05:48:53 -0400

    define the structure of dashboard pages

---
+++ src/DemoPages/Dashboards/Basic/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class BasicDashboard extends Component {
+  render() {
+    return <div>BasicDashboard works!</div>;
+  }
+}
+
+export default BasicDashboard;
--- src/DemoPages/Dashboards/index.js
+++ src/DemoPages/Dashboards/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+
+import AppHeader from '../../Layout/AppHeader';
+import AppSidebar from '../../Layout/AppSidebar';
+import AppFooter from '../../Layout/AppFooter';
+import BasicDashboard from './Basic';
 
 const Dashboards = () => {
   return (
-    <div>Dashboards works!</div>
+    <Fragment>
+      <AppHeader />
+
+      <div className="app-main">
+        <AppSidebar />
+
+        <div className="app-main__outer">
+          <div className="app-main__inner">
+            <Route path="/dashboards/basic" component={BasicDashboard} />
+          </div>
+
+          <AppFooter />
+        </div>
+      </div>
+    </Fragment>
   );
-}
- 
+};
+
 export default Dashboards;
