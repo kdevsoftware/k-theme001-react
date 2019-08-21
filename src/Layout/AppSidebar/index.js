0c0d074609a8242e3febfe0763659704bf966c78
kdev <kdev.software1110@gmail.com>
2019-08-21 07:31:55 -0400

    AppSidebar visualization

--- src/Layout/AppSidebar/index.js
+++ src/Layout/AppSidebar/index.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+
+import HeaderLogo from '../AppLogo';
+import Nav from '../AppNav/VerticalNavWrapper';
+import backgroundImage from '../../assets/images/sidebar/city1.jpg';
 
 class AppSidebar extends Component {
-  render() { 
+  render() {
     return (
-      <div>AppSidebar works!</div>
+      <Fragment>
+        {/* <div className="sidebar-mobile-overlay" /> */}
+
+        <div className="app-sidebar bg-royal sidebar-shadow">
+          <HeaderLogo />
+
+          <PerfectScrollbar>
+            <div className="app-sidebar__inner">
+              <Nav />
+            </div>
+          </PerfectScrollbar>
+
+          <div
+            className="app-sidebar-bg opacity-06"
+            style={{
+              backgroundImage: `url(${backgroundImage})`
+            }}
+          />
+        </div>
+      </Fragment>
     );
   }
 }
- 
+
 export default AppSidebar;
