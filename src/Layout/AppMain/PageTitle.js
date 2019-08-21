c000c35ebb2a3a5f046517870fbc68018a7ff177
kdev <kdev.software1110@gmail.com>
2019-08-21 13:11:15 -0400

    PageTitle visualization

--- src/Layout/AppMain/PageTitle.js
+++ src/Layout/AppMain/PageTitle.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
+import TitleComponent2 from './PageTitleExamples/Variation2';
+
 class PageTitle extends Component {
-  render() { 
+  render() {
     return (
-      <div>PageTitle works!</div>
+      <div className="app-page-title">
+        <div className="page-title-wrapper">
+          <div className="page-title-heading">
+            <div className="page-title-icon">
+              <i className="pe-7s-car icon-gradient bg-mean-fruit" />
+            </div>
+            <div>
+              Basic Dashboard
+              <div className="page-title-subheading">
+                This is an example dashboard created using build-in elements and
+                components.
+              </div>
+            </div>
+          </div>
+          <div className="page-title-actions">
+            <TitleComponent2 />
+          </div>
+        </div>
+      </div>
     );
   }
 }
- 
+
 export default PageTitle;
