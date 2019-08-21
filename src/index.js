426f7be37a4097c32ddfb65fc394968e919cfbb9
kdev <kdev.software1110@gmail.com>
2019-08-21 06:18:52 -0400

    add main style and required assets

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
+import './assets/base.css';
 import * as serviceWorker from './serviceWorker';
 import Main from './DemoPages/Main';
 
