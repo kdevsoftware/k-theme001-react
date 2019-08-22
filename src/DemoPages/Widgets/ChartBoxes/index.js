ede520edfecfa15be70c4138ea29857ea49f50c4
kdev <kdev.software1110@gmail.com>
2019-08-21 21:15:09 -0400

    define the dashboard-boxes page structure

---
+++ src/DemoPages/Widgets/ChartBoxes/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class WidgetsChartsBoxes extends React.Component {
+  render() {
+    return <div>WidgetsChartsBoxes works!</div>;
+  }
+}
+
+export default WidgetsChartsBoxes;
--- src/DemoPages/Widgets/index.js
+++ src/DemoPages/Widgets/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+
+import AppHeader from '../../Layout/AppHeader';
+import AppSidebar from '../../Layout/AppSidebar';
+import AppFooter from '../../Layout/AppFooter';
+import WidgetsChartsBoxes from './ChartBoxes';
 
 const Widgets = () => {
   return (
-    <div>Widgets works!</div>
+    <Fragment>
+      <AppHeader />
+
+      <div className="app-main">
+        <AppSidebar />
+
+        <div className="app-main__outer">
+          <div className="app-main__inner">
+            <Route
+              path="/widgets/dashboard-boxes"
+              component={WidgetsChartsBoxes}
+            />
+          </div>
+
+          <AppFooter />
+        </div>
+      </div>
+    </Fragment>
   );
-}
- 
+};
+
 export default Widgets;
