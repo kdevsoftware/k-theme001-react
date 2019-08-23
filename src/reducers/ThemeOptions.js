ee826feeccb400b10285710bd619c3a8fa19d098
kdev <kdev.software1110@gmail.com>
2019-08-23 08:13:14 -0400

    implement action handler

--- src/reducers/ThemeOptions.js
+++ src/reducers/ThemeOptions.js
@@ -141,6 +141,107 @@ export default function reducer(
   action
 ) {
   switch (action.type) {
+    case SET_ENABLE_BACKGROUND_IMAGE:
+      return {
+        ...state,
+        enableBackgroundImage: action.enableBackgroundImage
+      };
+
+    case SET_ENABLE_FIXED_HEADER:
+      return {
+        ...state,
+        enableFixedHeader: action.enableFixedHeader
+      };
+
+    case SET_ENABLE_HEADER_SHADOW:
+      return {
+        ...state,
+        enableHeaderShadow: action.enableHeaderShadow
+      };
+
+    case SET_ENABLE_SIDEBAR_SHADOW:
+      return {
+        ...state,
+        enableSidebarShadow: action.enableSidebarShadow
+      };
+
+    case SET_ENABLE_PAGETITLE_ICON:
+      return {
+        ...state,
+        enablePageTitleIcon: action.enablePageTitleIcon
+      };
+
+    case SET_ENABLE_PAGETITLE_SUBHEADING:
+      return {
+        ...state,
+        enablePageTitleSubheading: action.enablePageTitleSubheading
+      };
+
+    case SET_ENABLE_PAGE_TABS_ALT:
+      return {
+        ...state,
+        enablePageTabsAlt: action.enablePageTabsAlt
+      };
+
+    case SET_ENABLE_FIXED_SIDEBAR:
+      return {
+        ...state,
+        enableFixedSidebar: action.enableFixedSidebar
+      };
+
+    case SET_ENABLE_MOBILE_MENU:
+      return {
+        ...state,
+        enableMobileMenu: action.enableMobileMenu
+      };
+
+    case SET_ENABLE_MOBILE_MENU_SMALL:
+      return {
+        ...state,
+        enableMobileMenuSmall: action.enableMobileMenuSmall
+      };
+
+    case SET_ENABLE_CLOSED_SIDEBAR:
+      return {
+        ...state,
+        enableClosedSidebar: action.enableClosedSidebar
+      };
+
+    case SET_ENABLE_FIXED_FOOTER:
+      return {
+        ...state,
+        enableFixedFooter: action.enableFixedFooter
+      };
+
+    case SET_BACKGROUND_COLOR:
+      return {
+        ...state,
+        backgroundColor: action.backgroundColor
+      };
+
+    case SET_HEADER_BACKGROUND_COLOR:
+      return {
+        ...state,
+        headerBackgroundColor: action.headerBackgroundColor
+      };
+
+    case SET_COLOR_SCHEME:
+      return {
+        ...state,
+        colorScheme: action.colorScheme
+      };
+
+    case SET_BACKGROUND_IMAGE:
+      return {
+        ...state,
+        backgroundImage: action.backgroundImage
+      };
+
+    case SET_BACKGROUND_IMAGE_OPACITY:
+      return {
+        ...state,
+        backgroundImageOpacity: action.backgroundImageOpacity
+      };
   }
 
   return state;
