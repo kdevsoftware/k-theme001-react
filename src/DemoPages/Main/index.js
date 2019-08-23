ba4bfbf57d2ebfd232805bdc8d5878dbf55b40b2
kdev <kdev.software1110@gmail.com>
2019-08-23 14:53:25 -0400

    update main demo page component

--- src/DemoPages/Main/index.js
+++ src/DemoPages/Main/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import cx from 'classnames';
+
 import AppMain from '../../Layout/AppMain';
 
 class Main extends React.Component {
   render() {
+    let {
+      colorScheme,
+      enableFixedHeader,
+      enableFixedSidebar,
+      enableFixedFooter,
+      enableClosedSidebar,
+      closedSmallerSidebar,
+      enableMobileMenu,
+      enablePageTabsAlt
+    } = this.props;
+
     return (
-      <div className="app-container app-theme-white fixed-header fixed-sidebar fixed-footer">
-        <AppMain />
-      </div>
+      <Fragment>
+        <div
+          className={cx(
+            'app-container app-theme-' + colorScheme,
+            { 'fixed-header': enableFixedHeader },
+            { 'fixed-sidebar': enableFixedSidebar /* || width < 1250 */ },
+            { 'fixed-footer': enableFixedFooter },
+            { 'closed-sidebar': enableClosedSidebar /* || width < 1250 */ },
+            {
+              'closed-sidebar-mobile': closedSmallerSidebar /* || width < 1250 */
+            },
+            { 'sidebar-mobile-open': enableMobileMenu }
+          )}
+        >
+          <AppMain />
+        </div>
+      </Fragment>
     );
   }
 }
 
-export default Main;
+const mapStateToProp = state => ({
+  colorScheme: state.ThemeOptions.colorScheme,
+  enableFixedHeader: state.ThemeOptions.enableFixedHeader,
+  enableMobileMenu: state.ThemeOptions.enableMobileMenu,
+  enableFixedFooter: state.ThemeOptions.enableFixedFooter,
+  enableFixedSidebar: state.ThemeOptions.enableFixedSidebar,
+  enableClosedSidebar: state.ThemeOptions.enableClosedSidebar,
+  enablePageTabsAlt: state.ThemeOptions.enablePageTabsAlt
+});
+
+export default withRouter(connect(mapStateToProp)(Main));
