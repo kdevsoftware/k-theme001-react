d85ccd801533cb8ed6c9d9907c994862c000c134
kdev <kdev.software1110@gmail.com>
2019-08-25 14:58:09 -0400

    elements/buttons-standard page

--- src/DemoPages/Elements/Button/Standard/index.js
+++ src/DemoPages/Elements/Button/Standard/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../../Layout/AppMain/PageTitle';
+import ButtonsStandardSolid from './Examples/Solid';
+
+const tabsContent = [
+  {
+    title: 'Solid',
+    content: <ButtonsStandardSolid />
+  }
+];
+
+function getTabs() {
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
+}
 
 class ButtonsStandard extends React.Component {
   render() {
-    return <div>ButtonsStandard works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Standard Buttons"
+          subheading="Wide selection of buttons that feature different styles for backgrounds, borders and hover options!"
+          icon="pe-7s-plane icon-gradient bg-tempting-azure"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
