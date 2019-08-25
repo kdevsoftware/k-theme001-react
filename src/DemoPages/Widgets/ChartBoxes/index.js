a79bdb777d98dabcff170ead8abe5adf9c06e866
kdev <kdev.software1110@gmail.com>
2019-08-25 11:59:34 -0400

    update dashboard boxes page animation

--- src/DemoPages/Widgets/ChartBoxes/index.js
+++ src/DemoPages/Widgets/ChartBoxes/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import PageTitle from '../../../Layout/AppMain/PageTitle';
 import BasicExample from './Examples/Basic';
@@ -34,12 +35,21 @@ class WidgetsChartsBoxes extends React.Component {
           icon="pe-7s-star icon-gradient bg-ripe-malin"
         />
 
-        <Tabs
-          tabsWrapperClass="body-tabs body-tabs-layout"
-          transform={false}
-          showInkBar={true}
-          items={getTabs()}
-        />
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
       </Fragment>
     );
   }
