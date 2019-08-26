3aafa86250a482dd28f8c9d3198c23ca320740b5
kdev <kdev.software1110@gmail.com>
2019-08-25 22:04:53 -0400

    elements/navigation page

---
+++ src/DemoPages/Elements/Navs/Examples/NavVertical.js
@@ -0,0 +1,229 @@
+import React, { Fragment } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {
+  Button,
+  UncontrolledButtonDropdown,
+  DropdownToggle,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+import {
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+class NavsVertical extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  onMouseEnter() {
+    this.setState({ dropdownOpen: true });
+  }
+
+  onMouseLeave() {
+    this.setState({ dropdownOpen: false });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Row>
+            <Col md="12">
+              <Card className="main-card mb-3">
+                <CardBody>
+                  <CardTitle>Vertical Menu</CardTitle>
+                  <Row>
+                    <Col>
+                      <Nav vertical>
+                        <NavItem>
+                          <NavLink href="javascript:void(0);">Link</NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink href="javascript:void(0);">
+                            Link
+                            <div className="ml-auto badge badge-success">
+                              New
+                            </div>
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink href="javascript:void(0);">
+                            Another Link
+                            <div className="ml-auto badge badge-warning">
+                              512
+                            </div>
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink disabled href="javascript:void(0);">
+                            Disabled Link
+                          </NavLink>
+                        </NavItem>
+                      </Nav>
+                    </Col>
+                    <Col>
+                      <Nav vertical>
+                        <NavItem>
+                          <NavLink href="javascript:void(0);">
+                            <i className="nav-link-icon lnr-inbox"> </i>
+                            <span>Inbox</span>
+                            <div className="ml-auto badge badge-pill badge-secondary">
+                              86
+                            </div>
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink href="javascript:void(0);">
+                            <i className="nav-link-icon lnr-book"> </i>
+                            <span>Book</span>
+                            <div className="ml-auto badge badge-pill badge-danger">
+                              5
+                            </div>
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink href="javascript:void(0);">
+                            <i className="nav-link-icon lnr-picture"> </i>
+                            <span>Picture</span>
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink disabled href="javascript:void(0);">
+                            <i className="nav-link-icon lnr-file-empty"> </i>
+                            <span>File Disabled</span>
+                          </NavLink>
+                        </NavItem>
+                      </Nav>
+                    </Col>
+                  </Row>
+                  <div className="divider" />
+                  <div className="text-center">
+                    <UncontrolledButtonDropdown direction="down">
+                      <DropdownToggle
+                        caret
+                        className="mb-2 mr-2"
+                        color="primary"
+                      >
+                        Dropdown Basic
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <Nav vertical>
+                          <NavItem>
+                            <NavLink href="javascript:void(0);">Link</NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink href="javascript:void(0);">
+                              Link
+                              <div className="ml-auto badge badge-success">
+                                New
+                              </div>
+                            </NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink href="javascript:void(0);">
+                              Another Link
+                              <div className="ml-auto badge badge-warning">
+                                512
+                              </div>
+                            </NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink disabled href="javascript:void(0);">
+                              Disabled Link
+                            </NavLink>
+                          </NavItem>
+                        </Nav>
+                      </DropdownMenu>
+                    </UncontrolledButtonDropdown>
+                    <Dropdown
+                      direction="down"
+                      className="d-inline-block"
+                      onMouseOver={this.onMouseEnter}
+                      onMouseLeave={this.onMouseLeave}
+                      isOpen={this.state.dropdownOpen}
+                      toggle={this.toggle}
+                    >
+                      <DropdownToggle
+                        caret
+                        color="primary"
+                        className="mb-2 mr-2"
+                      >
+                        Dropdown Hover
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <Nav vertical>
+                          <NavItem>
+                            <NavLink href="javascript:void(0);">
+                              <i className="nav-link-icon lnr-inbox"> </i>
+                              <span>Inbox</span>
+                              <div className="ml-auto badge badge-pill badge-secondary">
+                                86
+                              </div>
+                            </NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink href="javascript:void(0);">
+                              <i className="nav-link-icon lnr-book"> </i>
+                              <span>Book</span>
+                              <div className="ml-auto badge badge-pill badge-danger">
+                                5
+                              </div>
+                            </NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink href="javascript:void(0);">
+                              <i className="nav-link-icon lnr-picture"> </i>
+                              <span>Picture</span>
+                            </NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink disabled href="javascript:void(0);">
+                              <i className="nav-link-icon lnr-file-empty"> </i>
+                              <span>File Disabled</span>
+                            </NavLink>
+                          </NavItem>
+                        </Nav>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
+  }
+}
+
+export default NavsVertical;
--- src/DemoPages/Elements/Navs/index.js
+++ src/DemoPages/Elements/Navs/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../Layout/AppMain/PageTitle';
+import NavsVertical from './Examples/NavVertical';
+import NavsHorizontal from './Examples/NavHorizontal';
+
+const tabsContent = [
+  {
+    title: 'Vertical Menus',
+    content: <NavsVertical />
+  },
+  {
+    title: 'Horizontal Menus',
+    content: <NavsHorizontal />
+  }
+];
+
+function getTabs() {
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
+}
 
 class NavigationExample extends React.Component {
   render() {
-    return <div>NavigationExample works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Navigation Menus"
+          subheading="Navigation menus are one of the basic building blocks for any web or mobile app."
+          icon="pe-7s-photo-gallery icon-gradient bg-mean-fruit"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
