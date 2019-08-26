fc0588bc7dc228cc6f705f7fb30e8eeaad5ff8cf
kdev <kdev.software1110@gmail.com>
2019-08-26 00:58:45 -0400

    forms/validation page

--- src/DemoPages/Forms/Elements/Validation/index.js
+++ src/DemoPages/Forms/Elements/Validation/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../../Layout/AppMain/PageTitle';
+import FormsFeedback from './Examples/Feedback';
+
+const tabsContent = [
+  {
+    title: 'Feedback',
+    content: <FormsFeedback />
+  }
+];
+
+function getTabs() {
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
+}
 
 class FormElementsValidation extends React.Component {
   render() {
-    return <div>FormElementsValidation works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Form Validation"
+          subheading="Inline validation is very easy to implement using the kdev-theme001-react."
+          icon="lnr-picture text-danger"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
