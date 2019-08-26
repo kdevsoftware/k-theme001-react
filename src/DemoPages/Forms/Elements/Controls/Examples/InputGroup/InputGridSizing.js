47837621f03c2927a85b9559008973cb5a520be0
kdev <kdev.software1110@gmail.com>
2019-08-26 00:15:08 -0400

    forms/controls page

---
+++ src/DemoPages/Forms/Elements/Controls/Examples/InputGroup/InputGridSizing.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
+
+class InputGridSizing extends React.Component {
+  render() {
+    return (
+      <Form>
+        <FormGroup row>
+          <Label for="exampleEmail" sm={2} size="lg">
+            Email
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="lg"
+              bsSize="lg"
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="exampleEmail2" sm={2}>
+            Email
+          </Label>
+          <Col sm={10}>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail2"
+              placeholder="default"
+            />
+          </Col>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+export default InputGridSizing;
--- src/DemoPages/Forms/Elements/Controls/index.js
+++ src/DemoPages/Forms/Elements/Controls/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../../Layout/AppMain/PageTitle';
+import FormsDefault from './Examples/FormBasic';
+import InputGroups from './Examples/InputGroup/InputGroups';
+
+const tabsContent = [
+  {
+    title: 'Basic',
+    content: <FormsDefault />
+  },
+  {
+    title: 'Input Groups',
+    content: <InputGroups />
+  }
+];
+
+function getTabs() {
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
+}
 
 class FormElementsControls extends React.Component {
   render() {
-    return <div>FormElementsControls works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Form Controls"
+          subheading="Wide selection of forms controls, using the Bootstrap 4 code base, but built with React."
+          icon="pe-7s-display1 icon-gradient bg-premium-dark"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
