3317f46806a1d8837b663bfe7678223df88d8c7f
kdev <kdev.software1110@gmail.com>
2019-08-26 00:23:21 -0400

    forms/layouts page

--- src/DemoPages/Forms/Elements/Layouts/index.js
+++ src/DemoPages/Forms/Elements/Layouts/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../../Layout/AppMain/PageTitle';
+import FormGrid from './Examples/FormGrid';
+import FormGridFormRow from './Examples/FormGridFormRow';
+
+const tabsContent = [
+  {
+    title: 'Layout ',
+    content: <FormGridFormRow />
+  },
+  {
+    title: 'Grid',
+    content: <FormGrid />
+  }
+];
+
+function getTabs() {
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
+}
 
 class FormElementsLayouts extends React.Component {
   render() {
-    return <div>FormElementsLayouts works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Form Layouts"
+          subheading="Build whatever layout you need with our ArchitectUI framework."
+          icon="pe-7s-graph text-success"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
