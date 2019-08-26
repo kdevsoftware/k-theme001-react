0266174bbc584df9b46c000cc883a6609df89fe8
kdev <kdev.software1110@gmail.com>
2019-08-25 22:41:56 -0400

    components/notifications page

--- src/DemoPages/Components/Notifications/index.js
+++ src/DemoPages/Components/Notifications/index.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../Layout/AppMain/PageTitle';
+import ToastifyAlerts from './Examples/ToastifyAlerts';
+import BasicAlerts from './Examples/BasicAlerts';
 
 class NotificationsExamples extends React.Component {
   render() {
-    return <div>NotificationsExamples works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Notifications"
+          subheading="Notifications represent one of the best ways to give feedback for various users actions."
+          icon="pe-7s-glasses icon-gradient bg-love-kiss"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <div className="mbg-3 h-auto pl-0 pr-0 bg-transparent no-border card-header">
+            <div className="card-header-title fsize-2 text-capitalize font-weight-normal">
+              Toastify
+            </div>
+          </div>
+          <ToastifyAlerts />
+          <div className="mbg-3 h-auto pl-0 pr-0 bg-transparent no-border card-header">
+            <div className="card-header-title fsize-2 text-capitalize font-weight-normal">
+              Basic Bootstrap 4 Alerts
+            </div>
+          </div>
+          <BasicAlerts />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
