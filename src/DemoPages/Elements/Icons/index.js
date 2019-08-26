59f969cd5ac927e41d3771f05be14b4c8abd852d
kdev <kdev.software1110@gmail.com>
2019-08-25 20:04:32 -0400

    elements/icons page

--- src/DemoPages/Elements/Icons/index.js
+++ src/DemoPages/Elements/Icons/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Tabs from 'react-responsive-tabs';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+import PageTitle from '../../../Layout/AppMain/PageTitle';
+import FontAwesomeIconsExample from './Examples/FontAwesome';
+import Pe7IconsExample from './Examples/Pe7Icons';
+import LinearIconsExample from './Examples/LinearIcons';
+
+const tabsContent = [
+  {
+    title: 'Pe7 Icons',
+    content: <Pe7IconsExample />
+  },
+  {
+    title: 'FontAwesome',
+    content: <FontAwesomeIconsExample />
+  },
+  {
+    title: 'Linear Icons',
+    content: <LinearIconsExample />
+  }
+];
+
+function getTabs() {
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
+}
 
 class IconsExamples extends React.Component {
   render() {
-    return <div>IconsExamples works!</div>;
+    return (
+      <Fragment>
+        <PageTitle
+          heading="Icons"
+          subheading="Wide icons selection including from flag icons to FontAwesome and other icons libraries."
+          icon="pe-7s-phone icon-gradient bg-night-fade"
+        />
+
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Tabs
+            tabsWrapperClass="body-tabs body-tabs-layout"
+            transform={false}
+            showInkBar={true}
+            items={getTabs()}
+          />
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
   }
 }
 
