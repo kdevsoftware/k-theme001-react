1d3daabc14cf2d5784634dbcd27b8d984c9bc867
kdev <kdev.software1110@gmail.com>
2019-08-25 23:55:26 -0400

    tables/regular-tables page

--- src/DemoPages/Tables/index.js
+++ src/DemoPages/Tables/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
 
-const Tables = () => {
-  return (
-    <div>Tables works!</div>
-  );
-}
- 
+import RegularTables from './RegularTables';
+
+const Tables = ({ match }) => (
+  <Fragment>
+    <Route path={`${match.url}/regular-tables`} component={RegularTables} />
+  </Fragment>
+);
+
 export default Tables;
